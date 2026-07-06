f71724603dc762553c5f04486c24e2cc201594aa
icholahounye-ops <icholahounye@gmail.com>
2026-07-07 00:13:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+ const buttons = document.querySelectorAll('.domaine');
+  const descriptions = document.querySelectorAll('.description');
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const targetId = btn.dataset.target;
+      const isActive = btn.classList.contains('active');
+      buttons.forEach(b => b.classList.remove('active'));
+      descriptions.forEach(d => d.classList.remove('visible'));
+      if (!isActive) {
+        btn.classList.add('active');
+        document.getElementById(targetId).classList.add('visible');
+      }
+    });
+  });
+
+document.getElementById('form-contact').addEventListener('submit', function (e) {
+    e.preventDefault();
+    window.location.href = 'thankyou.html';
+  });
+  
+  document.querySelectorAll('.photo img').forEach(function (img) {
+      img.addEventListener('click', function () {
+        window.open(img.src, '_blank');
+      });
+    });
